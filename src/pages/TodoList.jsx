3e7b8010888fb8230c99b278755c0c97a436189e
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-11-10 10:04:19 +0700

    Membuat addTodo di todolist page

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -4,6 +4,7 @@ import Paper from "../components/Paper";
 import Header from "../components/Header";
 import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
+
 const TodoList = () => {
   const [todos, setTodos] = useState([
     { text: "Learning React" },
@@ -11,10 +12,18 @@ const TodoList = () => {
     { text: "Learning styling in React" }
   ]);
 
+  const addTodo = (value) => {
+    const addedTodo = [...todos, { text: value }];
+
+    setTodos(addedTodo);
+  };
+
+  console.log(todos);
+
   return (
     <Paper>
       <Header />
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
       <Todos todos={todos} />
     </Paper>
   );
