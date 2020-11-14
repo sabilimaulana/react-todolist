694934f2453b6c2962c59aac5365f7e1a64a56c4
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-11-14 19:03:14 +0700

    Membuat header dengan css module

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
 
-import Button from "./button/Button";
+import Button from "../button/Button";
+
+import styles from "./header.module.css";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
-    <section className="header">
+    <section className={styles.header}>
       <Button onClick={showAddToggle} text={showAdd ? "finish" : "Add"} />
-      <h1 className="header-title">Todo lists</h1>
+      <h1 className={styles.headerTitle}>Todo lists</h1>
       <Button onClick={clearTodos} text="Clear" color="red" align="right" />
     </section>
   );
