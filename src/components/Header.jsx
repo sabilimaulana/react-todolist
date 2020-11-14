5964feb42a40d26535d978e152851ac278b121b0
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-11-14 18:49:08 +0700

    Membuat button dengan css module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Button from "./Button";
+import Button from "./button/Button";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
     <section className="header">
-      {/* <button className="header-btn main-black-color" onClick={showAddToggle}>
-        {showAdd ? "Finish" : "add"}
-      </button> */}
       <Button onClick={showAddToggle} text={showAdd ? "finish" : "Add"} />
       <h1 className="header-title">Todo lists</h1>
-      {/* <button className="header-btn main-red-color" onClick={clearTodos}>
-        Clear
-      </button> */}
       <Button onClick={clearTodos} text="Clear" color="red" align="right" />
     </section>
   );
