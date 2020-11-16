197debda16012072aa08cd34b6af4b51c52a7cb4
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-11-16 08:57:10 +0700

    Implementasi layout ke todoform component

--- src/components/todoform/TodoForm.jsx
+++ src/components/todoform/TodoForm.jsx
@@ -3,8 +3,12 @@ import { jsx, useTheme } from "@emotion/react";
 import React from "react";
 import PropTypes from "prop-types";
 
+import Container from "../../layout/Container";
+
 // import styles from "./todoform.module.css";
 import * as styles from "./todoform.styles";
+import Item from "../../layout/Item";
+import Button from "../button/Button";
 
 const TodoForm = ({ addTodo, showAdd }) => {
   const [value, setValue] = React.useState("");
@@ -30,15 +34,21 @@ const TodoForm = ({ addTodo, showAdd }) => {
 
   if (showAdd) {
     return (
-      <section css={styles.add}>
-        <form css={styles.addForm} onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            css={styles.addInput({ theme })}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <button css={styles.addBtn({ theme })}>Add</button>
+      <section className="todoform-component">
+        <form onSubmit={handleFormSubmit}>
+          <Container alignItems="flex-start">
+            <Item flex={1} padding="0 0 0 16px">
+              <input
+                type="text"
+                css={styles.addInput({ theme })}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </Item>
+            <Item>
+              <Button text="add" />
+            </Item>
+          </Container>
         </form>
       </section>
     );
--- src/layout/Item.jsx
+++ src/layout/Item.jsx
@@ -3,18 +3,24 @@ import { jsx, css } from "@emotion/react";
 // import React from "react";
 import PropTypes from "prop-types";
 
-const Item = ({ children, flex, align }) => {
+const Item = ({ children, flex, align, padding }) => {
   return (
     <div
       className="flex-item"
       css={css`
         flex: ${flex};
         text-align: ${align};
+        padding: ${padding};
       `}
     >
       {children}
     </div>
   );
+};
+
+Item.defaultProps = {
+  align: "left",
+  padding: "unset"
 };
 
 Item.propTypes = {
@@ -23,7 +29,8 @@ Item.propTypes = {
     PropTypes.node
   ]),
   flex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  align: PropTypes.oneOf(["center", "left", "right", "justify"])
+  align: PropTypes.oneOf(["center", "left", "right", "justify"]),
+  padding: PropTypes.string
 };
 
 export default Item;
