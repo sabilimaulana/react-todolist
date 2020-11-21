80274f18b6ecf4180229c0015d9837cffcb76b52
sabilimaulana <63346492+sabilimaulana@users.noreply.github.com>
2020-11-21 11:11:21 +0700

    Membuat TodoListClass

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./styles.css";
 import { ThemeProvider } from "@emotion/react";
 
-import TodoList from "./pages/TodoList";
+// import TodoList from "./pages/TodoList";
+import TodoList from "./pages/TodoListClass";
 
 const theme = {
   color: {
